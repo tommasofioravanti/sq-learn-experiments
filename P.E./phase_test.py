1cc0a3924fd4ab17f7941869882b7ced7b085d23
tommasofioravanti <tommaso.fiora8@gmail.com>
2022-05-09 11:39:41 +0200

    Update phase_test.py

--- P.E./phase_test.py
+++ P.E./phase_test.py
@@ -2,26 +2,25 @@ from sklearn.QuantumUtility.Utility import *
 warnings.simplefilter("ignore")
 
 epsilon = 0.1
+omegas = [0.35, 0.1]
 
 median = False
 consistent_pe=False
-p_e = False
+p_e = True
 
 if median:
-    median = median_evaluation(phase_estimation, gamma=0.1, Q=5, omega=0.123, epsilon=0.01, nqubit=False,
-                           plot_distribution=False,
-                           success_prob='QAE')
+    median = median_evaluation(phase_estimation, gamma=0.1, Q=5, omega=0.1, epsilon=0.01, nqubit=False,
+                           plot_distribution=False)
     print('median evaluation:', median)
 
-thetas = [0.35, 0.1]
 if consistent_pe:
     print('Consistent_PE')
     for _ in range(5):
-        cpe_estimations = [consistent_phase_estimation(epsilon=epsilon, delta=0.1, omega=th) for th in thetas]
+        cpe_estimations = [consistent_phase_estimation(omega=o, epsilon=epsilon, delta=0.1) for o in omegas]
         print()
-        #print(cpe_estimations)
+        print(cpe_estimations)
 if p_e:
     for _ in range(5):
-        pe_estimations = [phase_estimation(omega=th, epsilon=epsilon,delta=0.1) for th in thetas]
+        pe_estimations = [phase_estimation(omega=o, epsilon=epsilon,delta=0.1) for o in omegas]
 
         print('P.E:', pe_estimations)
